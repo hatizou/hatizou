2ec071f4e49dbf56e7db07b9a5ffcbd3397dd704
西村卓也 <>
2019-01-26 12:02:59 +0900

    no message

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+    // Start is called before the first frame update
+    //private float speed;
+    public float speed;
+    void Start () {
+        // speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position += new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime * speed, 0f, 0f);
+        //Debug.Log (transform.position.x);
+    }
+}
